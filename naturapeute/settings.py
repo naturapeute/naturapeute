85151d5eca68d8f2b2cfe0f5eef30d825ca883d3
Vincent Agnano <vinyll@protonmail.com>
2020-10-12 05:31:36 +0200

    secret key generic settings

--- naturapeute/settings.py
+++ naturapeute/settings.py
@@ -12,15 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j3#s8p^o#hf(3-b6v^coxa27308aidk4ohj=_5u559-qgcof-#'
+SECRET_KEY = ''
 
 DEBUG = False
 
