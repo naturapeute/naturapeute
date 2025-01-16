0bdc5662243a18e7358b3a268f60171bea3e9ec5
tristanbantam <IlRossoeilNero88?>
2025-01-16 21:14:52 +0100

    style(apps): add module and class docstrings to apps.py

--- api/apps.py
+++ api/apps.py
@@ -1,5 +1,8 @@
+"""Django app configuration for the API application."""
+
 from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
+    """Configuration class for the API application."""
     name = 'api'
