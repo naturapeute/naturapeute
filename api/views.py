b070bec64f53b1c372762d2a9533e2d22ece3096
tristanbantam <IlRossoeilNero88?>
2025-01-16 21:49:44 +0100

    style(views): fix multiple linting issues in views.py
    - Add module, class and method docstrings
    - Fix import order and remove unused imports
    - Fix LazyEncoder implementation
    - Fix PatientView implementation
    - Remove commented code

--- api/views.py
+++ api/views.py
@@ -1,29 +1,33 @@
+"""Views for the API application."""
+
 import json
 from datetime import date
 
-from django.http.response import HttpResponse, JsonResponse
-from django.views.generic import View
+from django.core.serializers.json import DjangoJSONEncoder
 from django.http import JsonResponse
-from django.core import serializers
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
 
-from naturapeute.models import Therapist, Patient
-from django.core.serializers.json import DjangoJSONEncoder
+from naturapeute.models import Patient, Therapist
 
 
 class LazyEncoder(DjangoJSONEncoder):
+    """Custom JSON encoder for lazy-loaded Django objects."""
     def default(self, obj):
-        if isinstance(obj, YourCustomType):
-            return str(obj)
+        """Convert object to JSON serializable format."""
+        if hasattr(obj, 'to_json'):
+            return obj.to_json()
         return super().default(obj)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class TherapistView(View):
+    """View for handling Therapist-related requests."""
     model = Therapist
 
     def get(self, *args, **kwargs):
+        """Get therapist details by email."""
         therapist = Therapist.members.get(email=kwargs["email_or_pk"])
         office = therapist.offices.first()
         data = {
@@ -44,6 +48,7 @@ class TherapistView(View):
         return JsonResponse(data)
 
     def patch(self, *args, **kwargs):
+        """Update therapist and associated patients."""
         therapist = Therapist.members.get(pk=kwargs["email_or_pk"])
         data = json.loads(self.request.body)
         therapist.invoice_data = data
@@ -51,7 +56,7 @@ class TherapistView(View):
         for p in data["patients"]:
             p["birthdate"] = date.fromtimestamp(p["birthdate"] / 1000)
             if "id" in p:
-                patient = Patient.objects.filter(pk=p["id"]).update(**p)
+                Patient.objects.filter(pk=p["id"]).update(**p)
             else:
                 patient = Patient.objects.create(**p)
                 therapist.patients.add(patient)
@@ -59,22 +64,14 @@ class TherapistView(View):
 
 
 class PatientView(View):
-    def post(self, *args, **kwargs):
-        Patient.objects.create(therapist=therapist, **json.loads(self.request.body))
-
-
-#   app.patch(`${prefix}/therapist/:id`, async (req, res) => {
-#     const content = req.body.extraData
-#     if(!content) return res.status(403, 'No data to patch')
-
-#     const therapist = await Therapist.findById(req.params.id)
-#     if(!therapist) return res.status(404).send('Therapist not found')
-#     let data = await TherapistData.findOne({ therapistAirtableId: therapist.airtableId })
-#     if(!data) data = new TherapistData({ therapistAirtableId: therapist.airtableId })
-#     data.data = content
-#     await data.save()
-#     res.setHeader('Content-Type', 'application/json')
-#     const inst = await therapist.asObject()
-#     res.send(inst)
-#   })
-# }
+    """View for handling Patient-related requests."""
+    def post(self, request, *args, **kwargs):
+        """Create a new patient associated with a therapist."""
+        therapist_id = kwargs.get('therapist_id')
+        therapist = Therapist.members.get(pk=therapist_id)
+        return JsonResponse(
+            Patient.objects.create(
+                therapist=therapist,
+                **json.loads(request.body)
+            ).to_json()
+        )
