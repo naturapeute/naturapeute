5b22dca1461ee881de47a09a11c303034d34b1e5
tristanbantam <IlRossoeilNero88?>
2025-01-16 21:54:55 +0100

    style(blog): add docstrings to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
+"""Admin configuration for the blog application."""
+
 from django.contrib import admin
 from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 
@@ -6,9 +8,11 @@ from .models import Article, ArticleTag
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin, DynamicArrayMixin):
+    """Admin configuration for Article model."""
     pass
 
 
 @admin.register(ArticleTag)
 class ArticleTagAdmin(admin.ModelAdmin, DynamicArrayMixin):
+    """Admin configuration for ArticleTag model."""
     pass
