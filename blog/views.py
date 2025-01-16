939372dd31311917f03621c6751cd4f482353616
tristanbantam <IlRossoeilNero88?>
2025-01-16 22:05:29 +0100

    style(blog): add docstrings to views.py

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,18 @@
+"""Views for the blog application."""
+
 from django.views.generic import ListView, DetailView
 
 from .models import Article
 
 
 class HomeView(ListView):
+    """View for displaying the list of articles on the home page."""
     model = Article
     template_name = "blog/index.html"
     context_object_name = "articles"
 
 
 class ArticleView(DetailView):
+    """View for displaying a single article."""
     model = Article
     template_name = "blog/article.html"
