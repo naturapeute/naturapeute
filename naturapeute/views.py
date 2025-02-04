64ccb1c29c63d2bce38111005f233e57c89c2302
MicheleValsania <IlRossoeilNero88?>
2025-02-04 09:58:25 +0100

    style(views): add docstrings to views module and all view classes

--- naturapeute/views.py
+++ naturapeute/views.py
@@ -1,6 +1,7 @@
+"""Views for the naturapeute application."""
 from django.views.generic import TemplateView, ListView, View
 from django.views.generic.base import RedirectView, reverse
-from django.shortcuts import redirect, reverse
+from django.shortcuts import redirect
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.db.models import Q
 
@@ -10,10 +11,16 @@ from .models import Therapist, Symptom, Practice
 
 
 class HomeView(TemplateView):
+    """View for the home page."""
 
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
+        """Get context data for the home page template.
+        
+        Returns:
+            dict: Context containing therapists count, latest therapists and practices
+        """
         context = super().get_context_data(**kwargs)
         therapists = Therapist.members.all()
         context["therapistsCount"] = therapists.count()
@@ -23,23 +30,35 @@ class HomeView(TemplateView):
 
 
 class TherapistByPractice(ListView):
+    """View for listing therapists filtered by practice."""
     template_name = "therapists.html"
     model = Therapist
     context_object_name = "therapists"
     queryset = Therapist.members.all()
 
     def get_queryset(self, *args, **kwargs):
+        """Filter queryset by practice slug.
+        
+        Returns:
+            QuerySet: Filtered therapists queryset
+        """
         qs = super().get_queryset(*args, **kwargs)
         qs = qs.filter(practice__slug__in=self.kwargs.practice)
 
 
 class TherapistsView(ListView):
+    """View for listing all therapists with optional filtering."""
     template_name = "therapists.html"
     model = Therapist
     context_object_name = "therapists"
     queryset = Therapist.members.all()
 
     def get(self, request, *args, **kwargs):
+        """Handle GET requests with practice name filtering.
+        
+        Returns:
+            HttpResponse: Redirect to practice-specific view or default list view
+        """
         practice_name = self.request.GET.get("practice")
         if practice_name:
             try:
@@ -51,6 +70,11 @@ class TherapistsView(ListView):
         return super().get(request, *args, **kwargs)
 
     def get_queryset(self, *args, **kwargs):
+        """Filter queryset by practice and/or symptoms.
+        
+        Returns:
+            QuerySet: Filtered therapists queryset
+        """
         qs = super().get_queryset(*args, **kwargs)
         params = self.request.GET
         symptom_name = params.get("symptom")
@@ -65,9 +89,15 @@ class TherapistsView(ListView):
 
 
 class TherapistView(TemplateView):
+    """View for displaying detailed therapist information."""
     template_name = "therapist.html"
 
     def get_context_data(self, **kwargs):
+        """Get context data for the therapist detail template.
+        
+        Returns:
+            dict: Context containing therapist object
+        """
         slug = f"{self.kwargs['slug0']}/{self.kwargs['slug1']}"
         try:
             therapist = Therapist.mixed.get(slug=slug)
@@ -76,15 +106,26 @@ class TherapistView(TemplateView):
         return {"therapist": therapist}
 
     def get(self, request, *args, **kwargs):
+        """Handle GET requests with membership verification.
+        
+        Returns:
+            HttpResponse: Redirect to therapists list for pending members,
+                        or render detail template
+        """
         therapist = self.get_context_data()["therapist"]
         if therapist.membership == "pending":
             return HttpResponseRedirect(reverse("therapists"))
-        else:
-            return super().get(request, *args, **kwargs)
+        return super().get(request, *args, **kwargs)
 
 
 class TherapistVcardView(View):
+    """View for generating vCard downloads for therapists."""
     def get(self, *args, **kwargs):
+        """Generate and return vCard response for therapist.
+        
+        Returns:
+            HttpResponse: vCard file download response
+        """
         card = vobject.vCard()
         therapist = Therapist.mixed.get(
             slug=f"{self.kwargs['slug0']}/{self.kwargs['slug1']}"
@@ -109,6 +150,7 @@ class TherapistVcardView(View):
 
 
 class TherapistOldView(RedirectView):
+    """Legacy view for handling old therapist URLs."""
     def get_redirect_url(self, *args, **kwargs):
         """Return URL for redirecting old therapist URLs to new format."""
-        return f"/therapeutes/{kwargs['slug0']}/{kwargs['slug1']}/"
+        return f"/therapists/{kwargs['slug0']}/{kwargs['slug1']}/"
