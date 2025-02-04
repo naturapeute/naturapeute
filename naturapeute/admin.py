4e9ec57728a8828e71f244a019a5d41025ea8fbc
MicheleValsania <IlRossoeilNero88?>
2025-02-04 09:48:35 +0100

    style(admin): add docstrings and fix linting issues in admin.py

--- naturapeute/admin.py
+++ naturapeute/admin.py
@@ -1,23 +1,29 @@
+"""Admin configuration for naturapeute application."""
 from django.contrib import admin
-from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 from django.utils.html import format_html
+from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 
-from .models import Patient, Therapist, Symptom, Practice, Office, OfficePicture, Synonym, models
+from .models import (
+    Office, OfficePicture, Patient, Practice,
+    Symptom, Synonym, Therapist
+)
 
 
 class OfficePictureInline(admin.TabularInline):
+    """Admin inline for OfficePicture model."""
     model = OfficePicture
     exclude = ["uuid"]
 
 
-
 @admin.register(Office)
 class OfficeAdmin(admin.ModelAdmin):
+    """Admin configuration for Office model."""
     inlines = [OfficePictureInline]
     search_fields = ["therapist__firstname", "therapist__lastname", "city"]
 
 
 class OfficeInline(admin.StackedInline):
+    """Admin inline for Office model."""
     model = Office
     readonly_fields = ["country"]
     extra = 1
@@ -25,6 +31,7 @@ class OfficeInline(admin.StackedInline):
 
 @admin.register(Therapist)
 class TherapistAdmin(admin.ModelAdmin, DynamicArrayMixin):
+    """Admin configuration for Therapist model."""
     list_display = ["__str__", "image_tag", "agreements", "slug"]
     list_filter = ["is_certified", "membership"]
     search_fields = ["firstname", "lastname", "slug"]
@@ -32,21 +39,21 @@ class TherapistAdmin(admin.ModelAdmin, DynamicArrayMixin):
     inlines = [OfficeInline]
 
     def image_tag(self, obj):
-        return format_html(f'<img src="{obj.photo_url}" style="width: 45px; height:45px;" />')
+        """Generate HTML for displaying therapist photo in admin."""
+        return format_html(
+            f'<img src="{obj.photo_url}" style="width: 45px; height:45px;" />'
+        )
     image_tag.short_description = 'Photo'
-
-    # formfield_overrides = {
-    #     models.ManyToManyField: {'widget': models.fields.forms.widgets.CheckboxSelectMultiple }
-    # }
 
 
 @admin.register(Practice)
 class PracticeAdmin(admin.ModelAdmin, DynamicArrayMixin):
-    pass
+    """Admin configuration for Practice model."""
 
 
 @admin.register(Symptom)
 class SymptomAdmin(admin.ModelAdmin, DynamicArrayMixin):
+    """Admin configuration for Symptom model."""
     list_display = ["parent", "__str__"]
     list_display_links = ["__str__"]
     search_fields = ["name", "keywords"]
@@ -54,9 +61,10 @@ class SymptomAdmin(admin.ModelAdmin, DynamicArrayMixin):
 
 @admin.register(Synonym)
 class SynonymAdmin(admin.ModelAdmin, DynamicArrayMixin):
+    """Admin configuration for Synonym model."""
     list_display = ["__str__", "words"]
 
 
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin, DynamicArrayMixin):
-    pass
+    """Admin configuration for Patient model."""
